a13064795b5e7644a1700a58b0b5fc886d56c653
taras sev <taras@optimozg.com>
2020-05-07 17:13:46 +0300

    Create  form submit Thank you page

--- src/components/FormSubmitedPage.js
+++ src/components/FormSubmitedPage.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import Navigation from './Navigation';
+import Footer from './Footer';
 
 const FormSubmitedPage = () => {
-    return (<div>FormSubmitedPage</div>);
+    return (
+        <React.Fragment>
+          <Navigation />
+          <div className="ui container content vertical segment stripe">
+                <h1>Спасибо</h1>
+                <p>
+                    Мы получили ваше сообщение и в скором времени свяжемся с вами.
+                </p>
+            </div>
+          <Footer />
+        </React.Fragment>
+      );
 }
 
 export default FormSubmitedPage;
