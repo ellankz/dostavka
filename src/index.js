e75c36da0d1495b4b8a181e678e834a95ec5f0c2
ellankz <enelipa@gmail.com>
2020-05-27 15:15:35 +0300

    Setup Google Analytics

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, compose } from "redux";
+import ReactGA from 'react-ga';
 import App from "./components/App";
 import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers());
 
-
-
-
+ReactGA.initialize('UA-160996123-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
 <Provider store={store}>
